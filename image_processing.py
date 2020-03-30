51d10616c3531e812dea87bb898838e9ed5e9ddc
William Murari <williammu6@gmail.com>
2020-03-30 02:47:39 -0300

    rotating img and obtaining new roi after rotating

--- image_processing.py
+++ image_processing.py
@@ -1,5 +1,7 @@
 import cv2
+import math
 import numpy as np
+from scipy import ndimage
 from imutils import contours
 from model import predict
 
@@ -12,103 +14,75 @@ BLOCK_SIZE = 50
 THRESHOLD = 25
 
 
-def get_grid(img):
-
-    # TODO : Use hough lines to create mask and remove
-    #        lines of thresh image before getting numbers
-    global grid_mapping
-
+def get_roi(img):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    roi = img.copy()
+    thresh = cv2.adaptiveThreshold(blur, 255, 1, 1, 11, 2)
+    contours, _ = cv2.findContours(
+        thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    max_area = 0
+    mask = np.zeros((gray.shape), np.uint8)
+    best_cnt = np.array(0)
 
-    cv2.imshow('gray', gray)
+    for i in contours:
+        area = cv2.contourArea(i)
+        if area > 1000:
+            if area > max_area:
+                max_area = area
+                best_cnt = i
 
-    thresh = cv2.adaptiveThreshold(
-        gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 57, 5)
+    if best_cnt.any():
+        cv2.drawContours(mask, [best_cnt], 0, 255, -1)
+        cv2.drawContours(mask, [best_cnt], 0, 0, 2)
 
-    y, x, _ = img.shape
-    w_cell = x // 9
-    h_cell = y // 9
-    rect_cells = []
+        contours, _ = cv2.findContours(
+            mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        if len(contours) == 1:
+            area = cv2.contourArea(contours[0])
+            (x, y, w, h) = cv2.boundingRect(contours[0])
+            roi = img[y-5:y+h+5, x-5:x+w+5]
+            return roi, True
+    return img, False
 
-    cv2.imshow('thresh', thresh)
-    index = 0
 
-    mask = thresh.copy()
-    mask[:] = 255
+def adjust_rotation(gray_img):
 
-    for i in range(10):
-        cv2.line(mask, (int(i*w_cell), 0), (int(i*w_cell), y), (0, 0, 0), 3)
-        cv2.line(mask, (0, int(i*h_cell)), (x, int(i*h_cell)), (0, 0, 0), 3)
+    edges = cv2.Canny(gray_img, 90, 150, apertureSize=3)
+    kernel = np.ones((3, 3), np.uint8)
+    edges = cv2.dilate(edges, kernel, iterations=1)
+    kernel = np.ones((5, 5), np.uint8)
+    edges = cv2.erode(edges, kernel, iterations=1)
 
-    thresh = thresh & mask
+    lines = cv2.HoughLines(edges, 1, np.pi/180, 150)
 
-    for i in range(10):
-        for j in range(10):
-            x1, x2 = int(i*w_cell+2), int(i*w_cell-2)+w_cell
-            y1, y2 = int(j*h_cell+2), int(j*h_cell-2)+h_cell
+    angle = 0
+    angles = []
 
-            index += 1
+    if lines is None:
+        return None
 
-            # if np.mean(cell) > 10:
-            #     number = predict(cell)
+    for line in lines:
 
-            #     cv2.putText(img, str(number), (x1, y2),
-            #                 cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 1)
+        rho, theta = line[0]
+        a = np.cos(theta)
+        b = np.sin(theta)
+        x0 = a*rho
+        y0 = b*rho
+        x1 = int(x0 + 1000*(-b))
+        y1 = int(y0 + 1000*(a))
+        x2 = int(x0 - 1000*(-b))
+        y2 = int(y0 - 1000*(a))
 
-        # xl = 0+int(i*w_cell)
-        # y1, y2 = 0, y
-    cv2.imshow('mask', thresh)
+        angle = np.arctan2(y2 - y1, x2 - x1) * 180. / np.pi
+        if abs(angle) < 45:
+            angles.append(angle)
+
+    final_angle = np.mean(angles)
+    if not math.isnan(final_angle):
+        img = ndimage.rotate(img, final_angle)
+
     return img
-
-
-def get_sudoku(image):
-    sudoku = np.zeros((9, 9), dtype=np.int8)
-
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    thresh = cv2.adaptiveThreshold(
-        gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 57, 5)
-
-    original_thresh = thresh.copy()
-    original_thresh = cv2.erode(original_thresh, (3, 3), -1)
-
-    cnts = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-
-    mask_numbers = thresh.copy()
-    mask_numbers[:] = 0
-
-    for c in cnts:
-        area = cv2.contourArea(c)
-        (x, y, w, h) = cv2.boundingRect(c)
-        if area > 80 and area < 1000:
-            cv2.rectangle(mask_numbers, (x+5, y+5), (x+w-5, y+h-5),
-                          (255, 255, 255), -1)
-
-    cv2.imshow('thresh', thresh)
-    cv2.imshow('mask', mask_numbers)
-
-    cnts = cv2.findContours(mask_numbers, cv2.RETR_TREE,
-                            cv2.CHAIN_APPROX_SIMPLE)
-    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-
-    cnts.sort(key=lambda x: get_contour_precedence(x, thresh.shape[1]))
-
-    cnts = cnts[:81]
-    for i, c in enumerate(cnts):
-        (x, y, w, h) = cv2.boundingRect(c)
-        number_image = original_thresh[y:y+h, x:x+w]
-        if np.mean(number_image) > 10:
-            prediction = predict(number_image)
-            sudoku[i//9, i % 9] = prediction
-        else:
-            grid_mapping[i] = {
-                'x': x,
-                'y': y,
-                'w': w,
-                'h': h,
-            }
-
-    return sudoku
 
 
 def plot_solution(image, solution):
--- main.py
+++ main.py
@@ -5,42 +5,28 @@ from image_processing import *
 
 cap = cv2.VideoCapture(0)
 while True:
-    _, image = cap.read()
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray, (5, 5), 0)
-    roi = image.copy()
-    thresh = cv2.adaptiveThreshold(blur, 255, 1, 1, 11, 2)
-    contours, _ = cv2.findContours(
-        thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    max_area = 0
-    mask = np.zeros((gray.shape), np.uint8)
-    best_cnt = np.array(0)
+    _, img = cap.read()
 
-    for i in contours:
-        area = cv2.contourArea(i)
-        if area > 1000:
-            if area > max_area:
-                max_area = area
-                best_cnt = i
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    if best_cnt.any():
-        cv2.drawContours(mask, [best_cnt], 0, 255, -1)
-        cv2.drawContours(mask, [best_cnt], 0, 0, 2)
+    # obter ROI contendo SUDOKU
+    roi, roi_found = get_roi(gray)
 
-        contours, _ = cv2.findContours(
-            mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        if len(contours) == 1:
-            area = cv2.contourArea(contours[0])
-            (x, y, w, h) = cv2.boundingRect(contours[0])
-            roi = image[y:y+h, x:x+w]
+    if roi_found:
+        # Rotationa a imagem baseado no ângulo das linhas
+        rotated = adjust_rotation(roi)
 
-            sudoku = get_grid(roi)
+        # Busca novo ROI removendo as bordas contidas na rotação
+
+        roi, found_roi = get_roi(rotated)
+
+        """
+        Obter o sudoku
+        """
 
     cv2.imshow("roi", roi)
 
     if cv2.waitKey(33) == ord('a'):
-        cv2.imwrite('cam_input.png', image)
-
-# print(sudoku)
+        cv2.imwrite('cam_input.png', img)
 
 cv2.destroyAllWindows()
