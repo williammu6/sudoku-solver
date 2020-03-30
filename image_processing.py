dfb2fa21e44d38a78e3ea9913e87bb7e29e15b8a
William Murari <williammu6@gmail.com>
2020-03-30 03:03:18 -0300

    rotation e roi ok

--- image_processing.py
+++ image_processing.py
@@ -7,12 +7,6 @@ from model import predict
 
 from sudoku_solver import Solver
 
-grid_mapping = {}
-
-
-BLOCK_SIZE = 50
-THRESHOLD = 25
-
 
 def get_roi(img):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -89,72 +83,3 @@ def adjust_rotation(img):
         img = ndimage.rotate(img, final_angle)
 
     return img
-
-
-def get_sudoku(image):
-    sudoku = np.zeros((9, 9), dtype=np.int8)
-
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    thresh = cv2.adaptiveThreshold(
-        gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 57, 5)
-
-    original_thresh = thresh.copy()
-    original_thresh = cv2.erode(original_thresh, (3, 3), -1)
-
-    cnts = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-
-    mask_numbers = thresh.copy()
-    mask_numbers[:] = 0
-
-    for c in cnts:
-        area = cv2.contourArea(c)
-        (x, y, w, h) = cv2.boundingRect(c)
-        if area > 80 and area < 1000:
-            cv2.rectangle(mask_numbers, (x+5, y+5), (x+w-5, y+h-5),
-                          (255, 255, 255), -1)
-
-    cv2.imshow('thresh', thresh)
-    cv2.imshow('mask', mask_numbers)
-
-    cnts = cv2.findContours(mask_numbers, cv2.RETR_TREE,
-                            cv2.CHAIN_APPROX_SIMPLE)
-    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-
-    cnts.sort(key=lambda x: get_contour_precedence(x, thresh.shape[1]))
-
-    cnts = cnts[:81]
-    for i, c in enumerate(cnts):
-        (x, y, w, h) = cv2.boundingRect(c)
-        number_image = original_thresh[y:y+h, x:x+w]
-        if np.mean(number_image) > 10:
-            prediction = predict(number_image)
-            sudoku[i//9, i % 9] = prediction
-        else:
-            grid_mapping[i] = {
-                'x': x,
-                'y': y,
-                'w': w,
-                'h': h,
-            }
-
-    return sudoku
-
-
-def plot_solution(image, solution):
-    FONT = cv2.FONT_HERSHEY_SIMPLEX
-    FONT_SCALE = cv2.FONT_HERSHEY_SIMPLEX
-    THICKNESS = 2
-
-    for i in range(81):
-        cell = grid_mapping.get(i, None)
-        if cell is None:
-            continue
-
-        text = str(solution[i//9][i % 9])
-        x, y, w, h = cell['x'], cell['y'], cell['w'], cell['h']
-        textsize = cv2.getTextSize(text, FONT, FONT_SCALE, THICKNESS)
-        position = (x, y+20)
-        cv2.putText(image, text, position, FONT,
-                    0.7, (255, 0, 0), THICKNESS)
-    return image
